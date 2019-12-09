3cd612e51469af0eb58698600e0af346b6a20d25
Bhavana M <bhavana2018@hotmail.com>
2019-12-09 16:14:16 -0500

    Add project files.

---
+++ MvcDemo/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemo.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            string wishes = "happy";
+            ViewBag.info = wishes;
+            return View();
+        }
+        public string Name()
+        {
+            return "Hello World!";
+        }
+        public RedirectResult GO()
+        {
+            return Redirect(@"D:\MVC CLASSES\MvcDemo\Views\Default\Index.cshtml");
+        }
+    }
+}
---
+++ WebApplication1/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            EmployeeModel obj = new EmployeeModel();
+            obj.name = "bhavana";
+            obj.Id = 1;
+            
+            return View(obj);
+
+        }
+
+    }
+}
